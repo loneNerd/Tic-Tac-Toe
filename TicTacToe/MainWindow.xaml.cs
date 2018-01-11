02609d876541333c36a4d68d404797535759da24
_loneNerd <alexander.the.great.21356@gmail.com>
2018-01-11 22:21:56 +0200

    Add some logics

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -22,12 +22,18 @@ namespace TicTacToe
     {
         bool isSingleplayer = true;
 
-        Button[] zones;
+        public static Button[][] zones;
 
         public MainWindow()
         {
             InitializeComponent();
-            zones = new Button[] { zone_1, zone_2, zone_3, zone_4, zone_5, zone_6, zone_7, zone_8, zone_9 };
+            zones = new Button[][]
+            {
+                new Button[] { zone_1, zone_2, zone_3 },
+                new Button[] { zone_4, zone_5, zone_6 },
+                new Button[] { zone_7, zone_8, zone_9 }
+            };
+            ProgrammLogic.CleanZone();
         }
         
         private void ZoneClick(object sender, RoutedEventArgs e)
@@ -35,30 +41,30 @@ namespace TicTacToe
             if (isSingleplayer)
                 ProgrammLogic.Singleplayer((Button)sender);
         }
-
-        private void NewGameClick(object sender, MouseButtonEventArgs e)
+        
+        private void GiveUp(object sender, MouseButtonEventArgs e)
         {
-            ProgrammLogic.NewGame(zones);
+            ProgrammLogic.GiveUp();
         }
 
         private void GULeave(object sender, MouseEventArgs e)
         {
-            ReduceText(GUEllipse, GULabel);
+            ReduceText(ref GUEllipse, ref GULabel);
         }
 
         private void GUMove(object sender, MouseEventArgs e)
         {
-            EnlargeText(GUEllipse, GULabel);
+            EnlargeText(ref GUEllipse, ref GULabel);
         }
 
-        void EnlargeText(Ellipse elps, Label lbl)
+        void EnlargeText(ref Ellipse elps, ref Label lbl)
         {
             lbl.FontSize = 30;
             elps.Height = 50;
             elps.Width = 150;
         }
 
-        void ReduceText(Ellipse elps, Label lbl)
+        void ReduceText(ref Ellipse elps, ref Label lbl)
         {
             lbl.FontSize = 25;
             elps.Height = 45;
--- TicTacToe/ProgrammLogic.cs
+++ TicTacToe/ProgrammLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace TicTacToe
@@ -7,31 +8,81 @@ namespace TicTacToe
     {
         static bool isX = true;
 
-        public static void Singleplayer(Button button)
+        public static void Singleplayer(Button curentZone)
         {
-            if (button.HasContent)
+            if ((char)curentZone.Content != ' ')
                 return;
             if (isX == true)
             {
-                button.Content = 'X';
-                button.Foreground = Brushes.Red;
+                isX = !isX;
+                curentZone.Content = 'X';
+                curentZone.Foreground = Brushes.Red;
+                ParseZone('X');
             }
             else
             {
-                button.Content = 'O';
-                button.Foreground = Brushes.Blue;
+                isX = !isX;
+                curentZone.Content = 'O';
+                curentZone.Foreground = Brushes.Blue;
+                ParseZone('O');
             }
-            isX = !isX;
         }
 
-        public static void NewGame(Button[] btns)
+        public static void CleanZone()
         {
-            for(uint i = 0; i < btns.Length; i++)
+            for(ushort i = 0; i < 3; i++)
             {
-                btns[i].Content = null;
-                btns[i].Background = Brushes.White;
+                for (ushort j = 0; j < 3; j++)
+                {
+                    MainWindow.zones[i][j].Content = ' ';
+                    MainWindow.zones[i][j].Background = Brushes.White;
+                }
             }
             isX = true;
+        }
+
+        public static void ParseZone(char symb)
+        {
+            char[][] btnText = new char[][]
+            {
+                new char[] { (char)MainWindow.zones[0][0].Content, (char)MainWindow.zones[0][1].Content, (char)MainWindow.zones[0][2].Content },
+                new char[] { (char)MainWindow.zones[1][0].Content, (char)MainWindow.zones[1][1].Content, (char)MainWindow.zones[1][2].Content },
+                new char[] { (char)MainWindow.zones[2][0].Content, (char)MainWindow.zones[2][1].Content, (char)MainWindow.zones[2][2].Content }
+
+            };
+
+            for (byte i = 0; i < btnText.GetLength(0); i++)
+            {
+                if ((btnText[i][0] == symb && btnText[i][1] == symb && btnText[i][2] == symb) |
+                    (btnText[0][i] == symb && btnText[1][i] == symb && btnText[2][i] == symb) |
+                    (i == 0 && btnText[i][i] == symb && btnText[i + 1][i + 1] == symb && btnText[i + 2][i + 2] == symb) |
+                    (i == 2 && btnText[i][i - 2] == symb && btnText[i - 1][i - 1] == symb && btnText[i - 2][i] == symb))
+                {
+                    MessageBox.Show(symb + " Win!");
+                    CleanZone();
+                    return;
+                }
+            }
+            if (btnText[0][0] != ' ' && btnText[0][1] != ' ' && btnText[0][2] != ' ' &&
+                btnText[1][0] != ' ' && btnText[1][1] != ' ' && btnText[1][2] != ' ' &&
+                btnText[2][0] != ' ' && btnText[2][1] != ' ' && btnText[2][2] != ' ')
+            {
+                MessageBox.Show("Draw");
+                CleanZone();
+            }
+        }
+
+        public static void GiveUp()
+        {
+            if (isX)
+            {
+                MessageBox.Show("X Give Up!");
+            }
+            else
+            {
+                MessageBox.Show("O Give Up!");
+            }
+            CleanZone();
         }
     }
 }
