81ef43ba36ecc57a417d6224ac9131fff64b8347
_loneNerd <alexander.the.great.21356@gmail.com>
2018-01-31 23:47:58 +0200

    Add some AI

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TicTacToe
@@ -20,33 +11,80 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool isSingleplayer = true;
+        bool isSingleplayer = false;
+        static uint xWins = 0;
+        static uint oWins = 0;
+        static uint draw = 0;
 
-        public static Button[][] zones;
+        public static Button[] zones;
+        static Label xWinsLabel;
+        static Label oWinsLabel;
+        static Label drawLabel;
+
+        //The next three properties I wrote just because
+        //I'm too lazy to write WinsLabel =
+        public static uint XWins
+        {
+            get { return xWins; }
+            set { xWins++;  xWinsLabel.Content = "X Wins: " + value.ToString(); }
+        }
+
+        public static uint OWins
+        {
+            get { return oWins; }
+            set { oWins++;  oWinsLabel.Content = "O Wins: " + value.ToString(); }
+        }
+
+        public static uint Draw
+        {
+            get { return draw; }
+            set { draw++;  drawLabel.Content = "Draw: " + value.ToString(); }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
-            zones = new Button[][]
-            {
-                new Button[] { zone_1, zone_2, zone_3 },
-                new Button[] { zone_4, zone_5, zone_6 },
-                new Button[] { zone_7, zone_8, zone_9 }
-            };
-            ProgrammLogic.CleanZone();
+            zones = new Button[] { zone_1, zone_2, zone_3, zone_4, zone_5, zone_6, zone_7, zone_8, zone_9 };
+            xWinsLabel = XWinsLabel;
+            oWinsLabel = OWinsLabel;
+            drawLabel = DrawLabel;
+            ProgrammLogics.CleanZone();
         }
         
         private void ZoneClick(object sender, RoutedEventArgs e)
         {
-            if (isSingleplayer)
-                ProgrammLogic.Singleplayer((Button)sender);
+            if ((char)((Button)sender).Content == ' ')
+            {
+                if (isSingleplayer)
+                {
+                    ProgrammLogics.Multiplayer((Button)sender);
+                }
+                else
+                {
+                    ((Button)sender).Content = AI.player;
+                    ((Button)sender).Foreground = AI.player == 'X' ? Brushes.Red : Brushes.Blue;
+                    if (ProgrammLogics.ParseDraw(ProgrammLogics.BtnConverter()))
+                    {
+                        ProgrammLogics.ParseZone(AI.player);
+                        return;
+                    }
+                    else
+                        ProgrammLogics.ParseZone(AI.player);
+                    AI.AITactics();
+                    ProgrammLogics.ParseZone(AI.ai);
+                }
+            }
         }
         
+        //Right name for this events "Surrender"
+        //I change this event twice and I'll not be any more
         private void GiveUp(object sender, MouseButtonEventArgs e)
         {
-            ProgrammLogic.GiveUp();
+            ProgrammLogics.Surrender();
         }
 
+        //Next two events and two methods just for style
+        //And increase the inscription of the button
         private void GULeave(object sender, MouseEventArgs e)
         {
             ReduceText(ref GUEllipse, ref GULabel);
--- TicTacToe/ProgrammLogic.cs
+++ TicTacToe/ProgrammLogic.cs
@@ -1,88 +1,98 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace TicTacToe
 {
-    class ProgrammLogic
+    class ProgrammLogics
     {
         static bool isX = true;
-
-        public static void Singleplayer(Button curentZone)
+        
+        //This method adds to the square the correct symbol of the correct color
+        public static void Multiplayer(Button curentZone)
         {
             if ((char)curentZone.Content != ' ')
                 return;
-            if (isX == true)
-            {
-                isX = !isX;
-                curentZone.Content = 'X';
-                curentZone.Foreground = Brushes.Red;
-                ParseZone('X');
-            }
-            else
-            {
-                isX = !isX;
-                curentZone.Content = 'O';
-                curentZone.Foreground = Brushes.Blue;
-                ParseZone('O');
-            }
+            curentZone.Content = isX ? 'X' : 'O';
+            curentZone.Foreground = isX ? Brushes.Red : Brushes.Blue;
+            isX = !isX;
+            ParseZone((char)curentZone.Content);
         }
 
+        //Just clean table
         public static void CleanZone()
         {
-            for(ushort i = 0; i < 3; i++)
-            {
-                for (ushort j = 0; j < 3; j++)
-                {
-                    MainWindow.zones[i][j].Content = ' ';
-                    MainWindow.zones[i][j].Background = Brushes.White;
-                }
-            }
+            Array.ForEach(MainWindow.zones, l => l.Content = ' ');
             isX = true;
         }
 
         public static void ParseZone(char symb)
         {
-            char[][] btnText = new char[][]
+            if (ParseWin(symb, BtnConverter()))
             {
-                new char[] { (char)MainWindow.zones[0][0].Content, (char)MainWindow.zones[0][1].Content, (char)MainWindow.zones[0][2].Content },
-                new char[] { (char)MainWindow.zones[1][0].Content, (char)MainWindow.zones[1][1].Content, (char)MainWindow.zones[1][2].Content },
-                new char[] { (char)MainWindow.zones[2][0].Content, (char)MainWindow.zones[2][1].Content, (char)MainWindow.zones[2][2].Content }
-
-            };
-
-            for (byte i = 0; i < btnText.GetLength(0); i++)
-            {
-                if ((btnText[i][0] == symb && btnText[i][1] == symb && btnText[i][2] == symb) |
-                    (btnText[0][i] == symb && btnText[1][i] == symb && btnText[2][i] == symb) |
-                    (i == 0 && btnText[i][i] == symb && btnText[i + 1][i + 1] == symb && btnText[i + 2][i + 2] == symb) |
-                    (i == 2 && btnText[i][i - 2] == symb && btnText[i - 1][i - 1] == symb && btnText[i - 2][i] == symb))
-                {
-                    MessageBox.Show(symb + " Win!");
-                    CleanZone();
-                    return;
-                }
+                if (symb == 'X')
+                    MainWindow.XWins++;
+                else
+                    MainWindow.OWins++;
+                MessageBox.Show(symb + " Win!");
+                CleanZone();
             }
-            if (btnText[0][0] != ' ' && btnText[0][1] != ' ' && btnText[0][2] != ' ' &&
-                btnText[1][0] != ' ' && btnText[1][1] != ' ' && btnText[1][2] != ' ' &&
-                btnText[2][0] != ' ' && btnText[2][1] != ' ' && btnText[2][2] != ' ')
+            else if (ParseDraw(BtnConverter()))
             {
+                MainWindow.Draw++;
                 MessageBox.Show("Draw");
                 CleanZone();
             }
         }
 
-        public static void GiveUp()
+        public static bool ParseWin(char symb, char[] zones)
+        {
+            //I wrote this, because in this way I can reduce the winner search algorythm
+            char[][] btnText = new char[][]
+                {
+                new char[] { zones[0], zones[1], zones[2] },
+                new char[] { zones[3], zones[4], zones[5] },
+                new char[] { zones[6], zones[7], zones[8] }
+                };
+
+            for (byte i = 0; i < btnText.GetLength(0); i++)
+                if ((btnText[i][0] == symb && btnText[i][1] == symb && btnText[i][2] == symb) ||
+                        (btnText[0][i] == symb && btnText[1][i] == symb && btnText[2][i] == symb) ||
+                        (i == 0 && btnText[i][i] == symb && btnText[i + 1][i + 1] == symb && btnText[i + 2][i + 2] == symb) ||
+                        (i == 2 && btnText[i][i - 2] == symb && btnText[i - 1][i - 1] == symb && btnText[i - 2][i] == symb))
+                    return true;
+            return false;
+        }
+
+        public static bool ParseDraw(char[] zones)
+        {
+            return Array.TrueForAll(zones, l => l != ' ');
+        }
+
+        public static void Surrender()
         {
             if (isX)
             {
-                MessageBox.Show("X Give Up!");
+                MainWindow.OWins++;
+                MessageBox.Show("X Surrender!");
             }
             else
             {
-                MessageBox.Show("O Give Up!");
+                MainWindow.XWins++;
+                MessageBox.Show("O Surrender!");
             }
             CleanZone();
+        }
+
+        public static char[] BtnConverter()
+        {
+            char[] charZones = new char[9];
+
+            for (int i = 0; i < MainWindow.zones.Length; i++)
+                charZones[i] = (char)MainWindow.zones[i].Content;
+
+            return charZones;
         }
     }
 }
